2663fe37aec30e925bd8f7b465fb9efdb4327395
buezrico <buezokoronkwo@gmail.com>
2025-04-13 01:35:46 +0000

    working on authentication using lucia

---
+++ components/layout/Header.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+const AnnouncementBar = () => (
+  <div className="w-full bg-black py-2">
+    <div className="container mx-auto flex items-center justify-center px-8">
+      <span className="text-center text-sm font-medium tracking-wide text-white">
+        FREE SHIPPING ON ORDERS OVER $15.00 • FREE RETURNS
+      </span>
+    </div>
+  </div>
+);
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const [prevScrollY, setPrevScrollY] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const scrolledUp = currentScrollY < prevScrollY;
+
+      if (scrolledUp) {
+        setIsOpen(true);
+      } else if (currentScrollY > 100) {
+        setIsOpen(false);
+      }
+
+      setPrevScrollY(currentScrollY);
+    };
+    setPrevScrollY(window.scrollY);
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollY]);
+
+  return (
+    <header className="w-full sticky top-0 z-50">
+      <div
+        className={`w-full transform transition-transform duration-300 ease-in-out ${
+          isOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <AnnouncementBar />
+
+        <div className="w-full flex justify-between items-center py-3 sm:py-4 bg-white/60 shadow-sm border-b border-gray-100 backdrop-blur-sm">
+          <div className="flex justify-between items-center container mx-auto px-8">
+            <div className="flex flex-1 justify-start items-center gap-4 sm:gap-6">
+              <button className="text-gray-700 hover:text-gray-900 md:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6 fill-none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                >
+                  <path strokeLinecap="round" d="M3 6h18M3 12h18M3 18h18" />
+                </svg>
+              </button>
+
+              <nav className="hidden md:flex gap-4 lg:gap-6 text-sm font-medium">
+                <Link href={"#"}>Shop</Link>
+                <Link href={"#"}>New Arrivals</Link>
+                <Link href={"#"}>Sale</Link>
+              </nav>
+            </div>
+
+            <Link href="#">Link</Link>
+
+            <div className="flex flex-1 justify-end items-center gap-2 sm:gap-4">
+              <button className="text-gray-700 hover:text-gray-900 sm:block">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6 fill-none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 21l-4.35-4.35M11 18a7 7 0 1 1 0-14 7 7 0 0 1 0 14z"
+                  />
+                </svg>
+              </button>
+
+              <Link href="/auth/sign-in">Sign In</Link>
+              <Link href="/auth/sign-up">Sign Up</Link>
+
+              <button className="text-gray-700 hover:text-gray-900 relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 sm:h-6 sm:w-6 fill-none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5 8h14l-1.5 11.5a1 1 0 0 1-1 .5H7.5a1 1 0 0 1-1-.5L5 8zM9 8V6a3 3 0 1 1 6 0v2"
+                  />
+                </svg>
+                <span className="absolute -top-0 -right-0.5 bg-black text-white text-[10px] sm:text-xs w-3.5 h-3.5 sm:w-4 sm:h-4 rounded-full flex items-center justify-center">
+                  0
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
